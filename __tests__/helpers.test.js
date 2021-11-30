61e92b030d329074e578333edb2516d94015020e
Adam <a.6942@yahoo.com>
2021-11-30 14:26:11 -0500

    updated connection file

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,7 @@
+const {format_date} = require('../utils/helpers')
+
+test('format_date() returns a date string', () => {
+    const date = new Date();
+    
+    expect(format_date(date)).toBe();
+});
